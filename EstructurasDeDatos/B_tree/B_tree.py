06e501aa81257a426bc58c1a2bb3e2354a07752c
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-18 10:01:27 -0500

    agregando la funcion insert_non_full() de la clase Btree

--- EstructurasDeDatos/B_tree/B_tree.py
+++ EstructurasDeDatos/B_tree/B_tree.py
@@ -45,3 +45,21 @@ class BTree:
         if not node.leaf:
             new_node.children = node.children[t:(2 * t)]
             node.children = node.children[0:t]
+
+    def insert_non_full(self, node, k):
+        i = len(node.keys) - 1
+        if node.leaf:
+            node.keys.append(0)
+            while i >= 0 and k < node.keys[i]:
+                node.keys[i + 1] = node.keys[i]
+                i -= 1
+            node.keys[i + 1] = k
+        else:
+            while i >= 0 and k < node.keys[i]:
+                i -= 1
+            i += 1
+            if len(node.children[i].keys) == 2 * self.t - 1:
+                self.split_child(node, i)
+                if k > node.keys[i]:
+                    i += 1
+            self.insert_non_full(node.children[i], k)
