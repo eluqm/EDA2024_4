3e7da4b23e85ae7420292556f121b92ccb7e7c11
SteveArms <acunoc@unsa.edu.pe>
2024-07-17 23:15:51 -0500

    Implementando logica para la eleccion aleatoria que se extraida del csv

--- Tests/selector.py
+++ Tests/selector.py
@@ -1,3 +1,17 @@
+import random
 class Selector:
     def __init__(self, canciones):
         self.galeria = canciones
+
+    def selectorMusics(self, numero):
+        songsSelect = []
+        indices_seleccionados = set()
+
+        while len(songsSelect) < numero:
+            indice_aleatorio = random.randint(0, len(self.galeria) - 1)
+            if indice_aleatorio not in indices_seleccionados:
+                indices_seleccionados.add(indice_aleatorio)
+                songsSelect.append(self.galeria[indice_aleatorio])
+
+        return songsSelect
+        
