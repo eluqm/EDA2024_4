c4708b0d04e68d0b89b9277bd13d936ea39c4ed2
SteveArms <acunoc@unsa.edu.pe>
2024-07-21 06:36:13 -0500

    Configuracion proyecto

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_episound',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR, "dist"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
