118e447be42669f52c05ba32430953737aaf5fcc
nuevo_63 <ajrahuacso@gmail.com>
2024-07-21 12:37:13 -0500

    Trie funcional, devuelve objetos ya no solo palabras

--- EstructurasDeDatos/Trie.py
+++ EstructurasDeDatos/Trie.py
@@ -3,49 +3,44 @@ from .LinkedList import LinkedList
 
 class TrieNode:
     def __init__(self):
-        # El HashMap usa caracteres como claves y los nodos hijos como valores.
         self.children = HashMap()  
-        # Marca si este nodo representa el final de una palabra.
         self.isWord = False
+        self.songs = LinkedList()
 
 class Trie:
     def __init__(self):
         # El Trie comienza con un nodo raíz vacío.
         self.root = TrieNode()
     
-    def insert(self, word):
+    # Inserta una palabra en el Trie.
+    def insert(self, word, song):
         current = self.root
         for char in word:
-            # Si el carácter no está en los hijos del nodo actual, se agrega un nuevo nodo.
             if not current.children.containsKey(char):
                 current.children.put(char, TrieNode())
-            # Mover al siguiente nodo según el carácter actual.
             current = current.children.get(char)
-        # Marca el final de la palabra.
         current.isWord = True
+        current.songs.add(song)
     
+    # Busca una palabra en el Trie.
     def search(self, word):
         current = self.root
         for char in word:
-            # Si el carácter no está en los hijos del nodo actual, la palabra no está en el Trie.
             if not current.children.containsKey(char):
-                return False
-            # Mover al siguiente nodo según el carácter actual.
+                return LinkedList() 
             current = current.children.get(char)
-        # Devuelve True si el nodo final de la palabra está marcado como verdadero.
-        return current.isWord
+        return current.songs if current.isWord else LinkedList()
     
+    # Verifica si hay alguna palabra en el Trie que empiece con el prefijo dado.
     def startsWith(self, prefix):
         current = self.root
         for char in prefix:
-            # Si el carácter del prefijo no está en los hijos del nodo actual, el prefijo no está en el Trie.
             if not current.children.containsKey(char):
                 return False
-            # Mover al siguiente nodo según el carácter actual.
             current = current.children.get(char)
-        # Si se recorren todos los caracteres del prefijo, se considera que el prefijo existe en el Trie.
         return True
     
+    # Busca todas las palabras que comienzan con el prefijo dado y las devuelve en una lista.
     def searchAll(self, prefix):
         current = self.root
         for char in prefix:
@@ -57,9 +52,11 @@ class Trie:
         self.searchDepth(current, prefix, words_list)
         return words_list
 
+    # Busca todas las palabras a partir del nodo dado y agrega las palabras encontradas a la lista.
     def searchDepth(self, node, prefix, words_list):
         if node.isWord:
-            words_list.add(prefix)
+            for song in node.songs:
+                words_list.add(song)
         
-        for char in node.children.entry_set():  # Usar entry_set() para obtener las claves del HashMap
+        for char in node.children.entry_set():
             self.searchDepth(node.children.get(char.key), prefix + char.key, words_list)
--- pruebaTriePrefijos.py
+++ pruebaTriePrefijos.py
@@ -2,17 +2,21 @@ from EstructurasDeDatos.Trie import Trie
 from leer_csv import leer_csv 
 
 def main():
-    archivo_csv = 'archive\\spotify_data.csv' 
+    archivo_csv = 'archive\\spotify_data.csv'
     canciones = leer_csv(archivo_csv, 100)
     trieArbol = Trie()
     
+    # Insertar las canciones en el Trie
     for cancion in canciones:
-        trieArbol.insert(cancion.get_track_name())
-
+        trieArbol.insert(cancion.get_track_name(), cancion)
+    
+    # Buscar canciones que empiezan con "W" (o cualquier otro prefijo para probar)
     resultado_busqueda = trieArbol.searchAll('W')
+    
+    # Imprimir el resultado de búsqueda
     print('Resultado de búsqueda para "W":')
-    for palabra in resultado_busqueda:
-        print(palabra)
-
+    for cancion in resultado_busqueda:
+        print(f' - {cancion}')
+        
 if __name__ == "__main__":
     main()
