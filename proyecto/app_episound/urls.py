aafa5e26b48f542e4c2a8cdaaaaed876367f3ae4
SteveArms <acunoc@unsa.edu.pe>
2024-07-21 08:45:55 -0500

    Incorporando paginas

--- proyecto/app_episound/urls.py
+++ proyecto/app_episound/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from app_episound import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('miMusica', views.miMusica, name="miMusica"),
+    path('buscador', views.buscar, name="buscar"),
+    path('reproduccion', views.reproducir, name="reproducir")
 ]
--- proyecto/app_episound/views.py
+++ proyecto/app_episound/views.py
@@ -4,4 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return render(request, "episound/home.html") 
+  return render(request, "inicio/page.html") 
+
+def miMusica(request):
+  return render(request, "miMusica/page.html")
+
+def buscar(request):
+  return render(request, "buscador/page.html")
+
+def reproducir(request):
+  return render(request, "reproduccion/page.html")
