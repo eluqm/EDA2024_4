2c4b3a54949afa37de73ad98553e54b2027e7718
nuevo_63 <ajrahuacso@gmail.com>
2024-07-21 10:16:41 -0500

    Factor de valance y rotaciones para el avl

--- EstructurasDeDatos/TreeAVL.py
+++ EstructurasDeDatos/TreeAVL.py
@@ -4,3 +4,35 @@ class Node:
         self.left = None
         self.right = None
         self.height = 1
+
+class AVLTree:
+    def _height(self, root):
+        if not root:
+            return 0
+        return root.height
+    
+    def _update_height(self, node):
+        node.height = 1 + max(self._height(node.left), self._height(node.right))
+
+    def balance_factor(self, node):
+            if not node:
+                return 0
+            return self._height(node.left) - self._height(node.right)
+    
+    def _rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+        x.right = y
+        y.left = T2
+        self._update_height(y)
+        self._update_height(x)
+        return x
+    
+    def _rotate_left(self, x):
+        y = x.right
+        T2 = y.left
+        y.left = x
+        x.right = T2
+        self._update_height(x)
+        self._update_height(y)
+        return y
