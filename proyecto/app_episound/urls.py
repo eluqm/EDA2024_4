36bc3f425b2cb8dd1eea9b6e598b12d04724eff8
SteveArms <acunoc@unsa.edu.pe>
2024-07-22 11:08:23 -0500

    Cambios en las urls

--- proyecto/app_episound/urls.py
+++ proyecto/app_episound/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from app_episound import views
 
 urlpatterns = [
-    path('', views.mostrar_cancion , name="index"),
+    path('', views.inicio, name="inicio"),
+    path('mostrarCancion', views.mostrar_cancion , name="index"),
     path('miMusica', views.miMusica, name="miMusica"),
     path('buscador', views.buscar, name="buscar"),
     path('reproduccion', views.reproducir, name="reproducir")
