a2674419ffea795fa86100e52cbcd7db35f5af74
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-22 02:00:10 -0500

    Probando la pagina

--- proyecto/app_episound/urls.py
+++ proyecto/app_episound/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app_episound import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.mostrar_cancion , name="index"),
     path('miMusica', views.miMusica, name="miMusica"),
     path('buscador', views.buscar, name="buscar"),
     path('reproduccion', views.reproducir, name="reproducir")
--- proyecto/app_episound/views.py
+++ proyecto/app_episound/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .in_memory_data import cancion
 # Create your views here.
 
 def index(request):
@@ -11,6 +11,12 @@ def miMusica(request):
 
 def buscar(request):
   return render(request, "buscador/page.html")
+
+def mostrar_cancion(request):
+  context = {
+      'cancion' : cancion
+  } 
+  return render(request, "inicio/page.html", context)
 
 def reproducir(request):
   return render(request, "reproduccion/page.html")
