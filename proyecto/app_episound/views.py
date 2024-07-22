eea25e8eb383d2ad8b96aa1cbce43dee9b64fb1e
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-22 04:42:01 -0500

    Agregando la vista mostrar_cancion para sacar los datos de la memoria y poder visualizarlos en la pagina home

--- proyecto/app_episound/views.py
+++ proyecto/app_episound/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .in_memory_data import cancion
+from .in_memory_data import canciones
 # Create your views here.
 
 def index(request):
@@ -13,10 +13,10 @@ def buscar(request):
   return render(request, "buscador/page.html")
 
 def mostrar_cancion(request):
-  context = {
-      'cancion' : cancion
-  } 
-  return render(request, "inicio/page.html", context)
+    context = {
+        'canciones': canciones
+    }
+    return render(request, "inicio/page.html", context)
 
 def reproducir(request):
   return render(request, "reproduccion/page.html")
