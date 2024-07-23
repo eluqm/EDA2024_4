671b9d729e53272ee22dee849498fdbfaf0f4f82
SteveArms <acunoc@unsa.edu.pe>
2024-07-22 21:53:38 -0500

    Agregando la ruta de la vista

--- proyecto/app_episound/urls.py
+++ proyecto/app_episound/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from app_episound import views
+from .views import *
 
 urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('mostrarCancion', views.mostrar_cancion , name="index"),
     path('miMusica', views.miMusica, name="miMusica"),
     path('buscador', views.buscar, name="buscar"),
-    path('reproduccion', views.reproducir, name="reproducir")
+    path('reproduccion', views.reproducir, name="reproducir"),
+    path('guardar-id/', guardar_id, name='guardar_id')
 ]
