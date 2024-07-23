0192f12e793d43c8cd66b9ef16236b485a5abf82
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-23 09:42:55 -0500

    Subiendo la estructura B-tree todavia faltan unas funciones para el orden

--- EstructurasDeDatos/B_tree.py
+++ EstructurasDeDatos/B_tree.py
@@ -1,10 +1,11 @@
+from .LinkedList import LinkedList
 class BTreeNode:
     def __init__(self, t, leaf=False):
         self.t = t  # Grado mínimo
         self.leaf = leaf  # Verdadero si el nodo es una hoja
         self.keys = []  # Lista de claves en el nodo
-        self.children = []  # Lista de hijos del nodo
-
+        self.children = []  # Lista de hijos del nodos
+        self.song_lists = []  # Lista de LinkedLists de canciones
 class BTree:
     def __init__(self, t):
         self.root = BTreeNode(t, True)  # Inicializa el árbol con una raíz vacía
@@ -23,16 +24,16 @@ class BTree:
         else:
             return self.search(k, node.children[i])
 
-    def insert(self, k):
+    def insert(self, k, song):
         root = self.root
         if len(root.keys) == 2 * self.t - 1:
             temp = BTreeNode(self.t)
             self.root = temp
             temp.children.insert(0, root)
             self.split_child(temp, 0)
-            self.insert_non_full(temp, k)
+            self.insert_non_full(temp, k, song)
         else:
-            self.insert_non_full(root, k)
+            self.insert_non_full(root, k, song)
 
     def split_child(self, parent, i):
         t = self.t
@@ -40,20 +41,27 @@ class BTree:
         new_node = BTreeNode(t, node.leaf)
         parent.children.insert(i + 1, new_node)
         parent.keys.insert(i, node.keys[t - 1])
+        parent.song_lists.insert(i, node.song_lists[t - 1])
         new_node.keys = node.keys[t:(2 * t - 1)]
+        new_node.song_lists = node.song_lists[t:(2 * t - 1)]
         node.keys = node.keys[0:(t - 1)]
+        node.song_lists = node.song_lists[0:(t - 1)]
         if not node.leaf:
             new_node.children = node.children[t:(2 * t)]
             node.children = node.children[0:t]
 
-    def insert_non_full(self, node, k):
+    def insert_non_full(self, node, k, song):
         i = len(node.keys) - 1
         if node.leaf:
-            node.keys.append(0)
             while i >= 0 and k < node.keys[i]:
-                node.keys[i + 1] = node.keys[i]
                 i -= 1
-            node.keys[i + 1] = k
+            if i >= 0 and k == node.keys[i]:
+                node.song_lists[i].add(song)
+            else:
+                node.keys.insert(i + 1, k)
+                song_list = LinkedList()
+                song_list.add(song)
+                node.song_lists.insert(i + 1, song_list)
         else:
             while i >= 0 and k < node.keys[i]:
                 i -= 1
@@ -62,7 +70,8 @@ class BTree:
                 self.split_child(node, i)
                 if k > node.keys[i]:
                     i += 1
-            self.insert_non_full(node.children[i], k)
+            self.insert_non_full(node.children[i], k, song)
+
     def print_tree(self, node=None, level=0):
         if node is None:
             node = self.root
