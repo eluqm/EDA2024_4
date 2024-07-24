bb5a4bbefdcd913572be59eaa8385788b64491b5
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-24 02:05:45 -0500

    Configurando las urls para mostrar las paginas

--- proyecto/app_episound/urls.py
+++ proyecto/app_episound/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from app_episound import views
 from .views import *
 
+
 urlpatterns = [
     path('', views.inicio, name="inicio"),
-    path('mostrarCancion', views.mostrar_cancion , name="index"),
+    path('mostrarCancion', views.mostrar_cancion, name="index"),
     path('miMusica', views.miMusica, name="miMusica"),
     path('buscador', views.buscar, name="buscar"),
     path('reproduccion', views.reproducir, name="reproducir"),
@@ -15,9 +16,13 @@ urlpatterns = [
     path('next_song/', views.next_song, name='next_song'),
     path('prev_song/', views.prev_song, name='prev_song'),
     path('play_btn/', views.play_song, name='play_song'),
-    path('miMusica/TiempodeDuracion/', views.TimeDuration, name="songs_duration"),
+    path('miMusica/TiempodeDuracion/', views.TimeDurationBtree, name="songs_duration"),
     path('miMusica/TiempodeDuracion/songs_ascending_Btree/', views.songs_ascend_btree , name='songs_ascending_Btree'),
     path('miMusica/TiempodeDuracion/songs_descending_Btree/', views.songs_descend_btree , name='songs_descending_Btree'),
-    path('random/', views.random, name ='random'),
-    #pones tus urls aca para año y popularidad
+    path('miMusica/Año/', views.TimeDurationAvl, name="songs_año"),
+    path('miMusica/Año/songs_ascend_avlAños/', views.songs_ascend_avlAños, name='songs_ascending_Avl'),
+    path('miMusica/Año/songs_descending_avlAños/', views.songs_descend_avlAños, name='songs_descending_Avl'),
+    path('random/', views.random, name='random'),
+    # Add more paths as needed
 ]
+
