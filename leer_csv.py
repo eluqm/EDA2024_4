cde73634e5c32be3b727d64fb0e997284d7f9b1d
VictorMA18 <vmamanian@unsa.edu.pe>
2024-07-26 15:42:54 -0500

    Modificando el leer csv y borrando el objeto Array

--- leer_csv.py
+++ leer_csv.py
@@ -1,33 +1,33 @@
 from cancion import Cancion
-from EstructurasDeDatos.Array import Array
 from EstructurasDeDatos.LinkedList import LinkedList
+from collections import namedtuple
 import csv
+import random
 
 def leer_csv(archivo_csv, n):
     canciones = LinkedList()
-    lista_canciones = []
+    canciones_lista = []
+
+    # Leer todas las canciones del archivo CSV
     with open(archivo_csv, newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-        contador = 0  
-
         for row in reader:
-            if contador < n:
-                # Crear una instancia de Cancion con los datos del CSV
-                cancion = Cancion(
-                    id=row['id'].strip(),
-                    artist_name=row['artist_name'].strip(),
-                    track_name=row['track_name'].strip(),
-                    track_year=row['year'].strip(),
-                    track_id=row['track_id'].strip(),
-                    track_popularity=int(row['popularity']),
-                    track_duration_ms=int(row['duration_ms'])
-                )
-                lista_canciones.append(cancion)
-                contador += 1 
-            else:
-                break 
-    
-    for cancion in lista_canciones:
+            cancion = Cancion(
+                id=row['id'].strip(),
+                artist_name=row['artist_name'].strip(),
+                track_name=row['track_name'].strip(),
+                track_year=row['year'].strip(),
+                track_id=row['track_id'].strip(),
+                track_popularity=int(row['popularity']),
+                track_duration_ms=int(row['duration_ms'])
+            )
+            canciones_lista.append(cancion)
+
+    # Seleccionar aleatoriamente n canciones sin repetir
+    canciones_seleccionadas = random.sample(canciones_lista, min(n, len(canciones_lista)))
+
+    # Agregar las canciones seleccionadas a la lista enlazada
+    for cancion in canciones_seleccionadas:
         canciones.add(cancion)
-    
+
     return canciones
--- proyecto/app_episound/in_memory_data.py
+++ proyecto/app_episound/in_memory_data.py
@@ -4,10 +4,8 @@ from .EstructurasDeDatos.Array import Array
 sys.path.append("../")
 from leer_csv import leer_csv
 
-# Crear instancias de Cancion
-numero = int(input("Ingresar las canciones: "))
-canciones = Array(int(numero))
 
 def datos():
+  numero = int(input("Ingresar las canciones: "))
   canciones = leer_csv("../archive/spotify_data.csv", numero)
   return canciones
