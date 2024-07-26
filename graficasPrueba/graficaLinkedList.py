c2511ec7dbb0c41d42023d68408776d15f283b6f
nuevo_63 <ajrahuacso@gmail.com>
2024-07-25 20:03:14 -0500

    Grafica para el Array

---
+++ graficasPrueba/graficaLinkedList.py
@@ -0,0 +1,24 @@
+from graficasPrueba.medirTiempoLinkedList import medir_tiempo_insercion
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    numeros_de_elementos = np.arange(1, 9001, 100)  
+    tiempos = []
+
+    for num_elementos in numeros_de_elementos:
+        elementos = list(range(num_elementos)) 
+        tiempo = medir_tiempo_insercion(elementos)
+        tiempos.append(tiempo)
+
+    plt.figure(figsize=(10, 6))
+    #Grafica de lineas: plt.plot(entradas, tiempos, marker='o', linestyle='-', color='b', label='Tiempo de Ejecución')
+    plt.bar(numeros_de_elementos, tiempos, color='skyblue', edgecolor='blue')
+    plt.xlabel('Número de Elementos')
+    plt.ylabel('Tiempo de Ejecución (segundos)')
+    plt.title('Eficiencia de Inserción en LinkedList')
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
